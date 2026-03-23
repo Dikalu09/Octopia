4e00b97137434b86105c0f8216ba24ec7bd41774
Trooki-05 <mamadou.sall@istlm.org>
2026-03-23 08:32:12 +0100

    Update jeux.js

--- Octopia/jeux.js
+++ Octopia/jeux.js
@@ -1,47 +1,39 @@
-function verifierMath() {
-    let rep0 = document.getElementById("m0").value;
-    let rep1 = document.getElementById("m1").value;
-    let rep2 = document.getElementById("m2").value;
-    let rep3 = document.getElementById("m3").value;
-    let rep4 = document.getElementById("m4").value;
-    let rep5 = document.getElementById("m5").value;
-    let rep6 = document.getElementById("m6").value;
-    let rep7 = document.getElementById("m7").value;
-    let rep8 = document.getElementById("m8").value;
-    let rep9 = document.getElementById("m9").value;
+const reponsesMath = ["12", "4", "9", "16", "10", "18", "50", "5", "16", "50"];
+const reponsesConju = ["mange", "finis", "parle", "sommes", "avez", "chantent", "joue", "allons", "es", "finissent"];
 
-    document.getElementById("rsm0").innerHTML = (rep0 == 12) ? "✔️" : "❌";
-    document.getElementById("rsm1").innerHTML = (rep1 == 6) ? "✔️" : "❌";
-    document.getElementById("rsm2").innerHTML = (rep2 == 9) ? "✔️" : "❌";
-    document.getElementById("rsm3").innerHTML = (rep3 == 16) ? "✔️" : "❌";
-    document.getElementById("rsm4").innerHTML = (rep4 == 10) ? "✔️" : "❌";
-    document.getElementById("rsm5").innerHTML = (rep5 == 18) ? "✔️" : "❌";
-    document.getElementById("rsm6").innerHTML = (rep6 == 50) ? "✔️" : "❌";
-    document.getElementById("rsm7").innerHTML = (rep7 == 5) ? "✔️" : "❌";
-    document.getElementById("rsm8").innerHTML = (rep8 == 16) ? "✔️" : "❌";
-    document.getElementById("rsm9").innerHTML = (rep9 == 50) ? "✔️" : "❌";
+let fautesM = new Array(10).fill(0);
+let fautesC = new Array(10).fill(0);
+
+function verifierMath() {
+    for (let i = 0; i < 10; i++) {
+        let saisie = document.getElementById("m" + i).value;
+        
+        tester(saisie, reponsesMath[i], "rsm" + i, i, fautesM);
+    }
 }
 
 function verifierConju() {
-    let c0 = document.getElementById("c0").value.toLowerCase();
-    let c1 = document.getElementById("c1").value.toLowerCase();
-    let c2 = document.getElementById("c2").value.toLowerCase();
-    let c3 = document.getElementById("c3").value.toLowerCase();
-    let c4 = document.getElementById("c4").value.toLowerCase();
-    let c5 = document.getElementById("c5").value.toLowerCase();
-    let c6 = document.getElementById("c6").value.toLowerCase();
-    let c7 = document.getElementById("c7").value.toLowerCase();
-    let c8 = document.getElementById("c8").value.toLowerCase();
-    let c9 = document.getElementById("c9").value.toLowerCase();
+    for (let i = 0; i < 10; i++) {
+        let saisie = document.getElementById("c" + i).value.toLowerCas
+        tester(saisie, reponsesConju[i], "rsc" + i, i, fautesC);
+    }
+}
 
-    document.getElementById("rsc0").innerHTML = (c0 == "mange") ? "✔️" : "❌";
-    document.getElementById("rsc1").innerHTML = (c1 == "finis") ? "✔️" : "❌";
-    document.getElementById("rsc2").innerHTML = (c2 == "parle") ? "✔️" : "❌";
-    document.getElementById("rsc3").innerHTML = (c3 == "sommes") ? "✔️" : "❌";
-    document.getElementById("rsc4").innerHTML = (c4 == "avez") ? "✔️" : "❌";
-    document.getElementById("rsc5").innerHTML = (c5 == "chantent") ? "✔️" : "❌";
-    document.getElementById("rsc6").innerHTML = (c6 == "joue") ? "✔️" : "❌";
-    document.getElementById("rsc7").innerHTML = (c7 == "allons") ? "✔️" : "❌";
-    document.getElementById("rsc8").innerHTML = (c8 == "es") ? "✔️" : "❌";
-    document.getElementById("rsc9").innerHTML = (c9 == "finissent") ? "✔️" : "❌";
+function tester(saisie, bonneRep, idSymbole, index, tabErreurs) {
+    let baliseB = document.getElementById(idSymbole);
+    
+    if (saisie === bonneRep) {
+        baliseB.innerText = "V ✅";
+        baliseB.style.color = "green";
+    } else if (saisie !== "") {
+        tabErreurs[index]++;
+        
+        if (tabErreurs[index] === 1) {
+            baliseB.innerText = "Try! 🧐";
+            baliseB.style.color = "orange";
+        } else {
+            baliseB.innerText = "X (" + bonneRep + ") ❌";
+            baliseB.style.color = "red";
+        }
+    }
 }
